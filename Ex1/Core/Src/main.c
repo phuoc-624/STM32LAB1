d9a75d6b36dc73d083a16ec0ec1c073ebbcce7b2
DESKTOP-0NSG2HJ\Admin <phuoc.nguyen622004@hcmut.edu.vn>
2025-09-17 23:23:45 +0700

    Update for Ex2

--- Ex1/Core/Src/main.c
+++ Ex1/Core/Src/main.c
@@ -93,23 +93,25 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  /*HAL_GPIO_WritePin ( LED_RED_GPIO_Port , LED_RED_Pin ,
+	  HAL_GPIO_WritePin ( LED_RED_GPIO_Port , LED_RED_Pin ,
 	  GPIO_PIN_RESET );
+	  HAL_GPIO_WritePin ( LED_YELLOW_GPIO_Port , LED_YELLOW_Pin ,
+	  GPIO_PIN_SET );
+	  HAL_GPIO_WritePin ( LED_BLUE_GPIO_Port , LED_BLUE_Pin ,
+	  GPIO_PIN_SET );
 	  HAL_Delay (5000);
+
 	  HAL_GPIO_WritePin ( LED_RED_GPIO_Port , LED_RED_Pin ,
 	  GPIO_PIN_SET );
-
 	  HAL_GPIO_WritePin ( LED_YELLOW_GPIO_Port , LED_YELLOW_Pin ,
 	  GPIO_PIN_RESET );
 	  HAL_Delay (2000);
+
 	  HAL_GPIO_WritePin ( LED_YELLOW_GPIO_Port , LED_YELLOW_Pin ,
 	  GPIO_PIN_SET );
-
 	  HAL_GPIO_WritePin ( LED_BLUE_GPIO_Port , LED_BLUE_Pin ,
 	  GPIO_PIN_RESET );
 	  HAL_Delay (3000);
-	  HAL_GPIO_WritePin ( LED_BLUE_GPIO_Port , LED_BLUE_Pin ,
-	  GPIO_PIN_SET );*/
 
     /* USER CODE END WHILE */
 
