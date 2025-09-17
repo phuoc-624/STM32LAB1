cbbbd4355ff9af163b21ac0913295ee552a28a50
DESKTOP-0NSG2HJ\Admin <phuoc.nguyen622004@hcmut.edu.vn>
2025-09-18 00:05:41 +0700

    Update for Ex2

--- Ex1/Core/Inc/main.h
+++ Ex1/Core/Inc/main.h
@@ -62,8 +62,8 @@ void Error_Handler(void);
 #define LED_RED_GPIO_Port GPIOA
 #define LED_YELLOW_Pin GPIO_PIN_6
 #define LED_YELLOW_GPIO_Port GPIOA
-#define LED_BLUE_Pin GPIO_PIN_7
-#define LED_BLUE_GPIO_Port GPIOA
+#define LED_GREEN_Pin GPIO_PIN_7
+#define LED_GREEN_GPIO_Port GPIOA
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
--- Ex1/Core/Src/main.c
+++ Ex1/Core/Src/main.c
@@ -97,7 +97,7 @@ int main(void)
 	  GPIO_PIN_RESET );
 	  HAL_GPIO_WritePin ( LED_YELLOW_GPIO_Port , LED_YELLOW_Pin ,
 	  GPIO_PIN_SET );
-	  HAL_GPIO_WritePin ( LED_BLUE_GPIO_Port , LED_BLUE_Pin ,
+	  HAL_GPIO_WritePin ( LED_GREEN_GPIO_Port , LED_GREEN_Pin ,
 	  GPIO_PIN_SET );
 	  HAL_Delay (5000);
 
@@ -109,7 +109,7 @@ int main(void)
 
 	  HAL_GPIO_WritePin ( LED_YELLOW_GPIO_Port , LED_YELLOW_Pin ,
 	  GPIO_PIN_SET );
-	  HAL_GPIO_WritePin ( LED_BLUE_GPIO_Port , LED_BLUE_Pin ,
+	  HAL_GPIO_WritePin ( LED_GREEN_GPIO_Port , LED_GREEN_Pin ,
 	  GPIO_PIN_RESET );
 	  HAL_Delay (3000);
 
@@ -168,10 +168,10 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, LED_RED_Pin|LED_YELLOW_Pin|LED_BLUE_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, LED_RED_Pin|LED_YELLOW_Pin|LED_GREEN_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : LED_RED_Pin LED_YELLOW_Pin LED_BLUE_Pin */
-  GPIO_InitStruct.Pin = LED_RED_Pin|LED_YELLOW_Pin|LED_BLUE_Pin;
+  /*Configure GPIO pins : LED_RED_Pin LED_YELLOW_Pin LED_GREEN_Pin */
+  GPIO_InitStruct.Pin = LED_RED_Pin|LED_YELLOW_Pin|LED_GREEN_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
